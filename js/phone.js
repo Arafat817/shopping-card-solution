684e49134f3d2f7307bf109b1a54f8882698a452
Arafat817 <gr1arafat@gmail.com>
2023-03-03 01:30:28 +0600

    shopping card complete

--- js/phone.js
+++ js/phone.js
@@ -1,37 +1,39 @@
-function updatePhoneNumber(isIncrise){
+function updatePhoneNumber (isIncrease){
     const phoneNumberField = document.getElementById('phone-number-field');
     const phoneNumberString = phoneNumberField.value;
     const previousPhoneNumber = parseInt(phoneNumberString);
 
     let newPhoneNumber;
-    if(isIncrise){
+
+    if(isIncrease){
         newPhoneNumber = previousPhoneNumber + 1;
     }
     else{
         newPhoneNumber = previousPhoneNumber - 1;
     }
     phoneNumberField.value = newPhoneNumber;
+    
     return newPhoneNumber;
-
 }
+
 function updatePhoneTotalPrice(newPhoneNumber){
-    const phoneTotalPrice = newPhoneNumber * 1220;
+    const phoneTotalPrice = newPhoneNumber * 1219;
     const phoneTotalElement = document.getElementById('phone-total');
-    phoneTotalElement.innerText = phoneTotalPrice; 
+    phoneTotalElement.innerText = phoneTotalPrice;
 }
 
 
-
-document.getElementById('btn-phone-pluse').addEventListener('click', function(){
-    newPhoneNumber= updatePhoneNumber (true);
-
+document.getElementById('btn-phone-plus').addEventListener('click', function(){
+    const newPhoneNumber = updatePhoneNumber(true);
+    
     updatePhoneTotalPrice(newPhoneNumber);
     calculateSubTotal();
-   
-})
+    
 
-document.getElementById('btn-phone-minuse').addEventListener('click', function(){
-    newPhoneNumber= updatePhoneNumber (false);
+});
+
+document.getElementById('btn-phone-minus').addEventListener('click', function(){
+    const newPhoneNumber = updatePhoneNumber(false);
 
     updatePhoneTotalPrice(newPhoneNumber);
     calculateSubTotal();
